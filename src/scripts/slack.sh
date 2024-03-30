4ba93802547d1c56ecc73bc7874a1967c5b153fb
devatherock <devatherock@gmail.com>
2024-03-30 08:29:55 -0500

    fix: Corrected String interpolation in curl with multiline string

--- src/scripts/slack.sh
+++ src/scripts/slack.sh
@@ -1,10 +1,14 @@
+template=$(cat <<-_EOT_
+{
+  "text": "$TEXT",
+  "channel": "$CHANNEL",
+  "color": "$COLOR",
+  "title": "$TITLE",
+  "webhook": "$SLACK_WEBHOOK",
+  "build_id": "$CIRCLE_WORKFLOW_ID"
+}
+_EOT_
+)
 curl --location 'https://simple-slack.fly.dev/api/notification' \
      --header 'Content-Type: application/json' \
-     --data '{
-        "text": "$TEXT",
-        "channel": "$CHANNEL",
-        "color": "$COLOR",
-        "title": "$TITLE",
-        "webhook": "$SLACK_WEBHOOK",
-        "build_id": "$CIRCLE_WORKFLOW_ID"
-      }'
+     --data "${template}"
